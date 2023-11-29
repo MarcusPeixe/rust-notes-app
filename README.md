16685129b3bd6917a6f03e9d6fb4368c8c50fdc8
MarcusPeixe <marcusv.peixoto23@gmail.com>
2023-11-29 03:13:26 -0300

    Updated README.md

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Rust Notes App
 
 This repo is just a little experiment, where I try to learn and improve my
-Rust.
+Rust. I also try to make the best out of cargo, learning all its options and
+functionalities in the way.
 
 ## Usage
 
 + `cargo r` or `cargo run` runs the project.
 + `cargo t` or `cargo test` runs unit tests.
++ `cargo d` or `cargo doc` builds the docs.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 //! # Rust Notes App
 //! 
 //! This repo is just a little experiment, where I try to learn and improve my
-//! Rust.
+//! Rust. I also try to make the best out of cargo, learning all its options
+//! and functionalities in the way.
 //! 
 //! ## Usage
 //! 
 //! + `cargo r` or `cargo run` runs the project.
 //! + `cargo t` or `cargo test` runs unit tests.
+//! + `cargo d` or `cargo doc` builds the docs.
 
 #![warn(missing_docs)]
 
 #[cfg(test)]
 mod tests;
 pub mod serial;
-pub mod user_session;
-pub mod app;
+mod user_session;
+mod app;
 
 #[doc(hidden)]
 fn main() {
